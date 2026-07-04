5528f7a1030527eec40c8a334fbb95fcc55f2ca5
ccrdferroviario <aruizrubio@gmail.com>
2026-07-04 20:39:51 +0200

    Update noticias.js

--- noticias.js
+++ noticias.js
@@ -1,4 +1,4 @@
-[
+const misNoticias = [
     {
         "seccion": "tiro",
         "titulo": "¡Gran éxito en la competición interempresas!",
@@ -14,6 +14,6 @@
         "resumen": "Hasta 11 corredores se han dado cita en la gran distancia del atletismo.", 
         "contenido": "Hasta 11 corredores se han dado cita en la gran distancia del atletismo. Morgaz, Dorado (3h16'29''), Cecilio (3h24'11''), Adorna (3h31'50''), Castilla (3h36'49''), Iván (3h40'58''), Ignacio (3h42'56''), Antonio Dominguez (3h46'15''), Jairo (4h05'07''), Antonio Rafael (4h24'27'') y Moises (5h12'39'') han completado los 42,195km en un magnífico recorrido por las calles de Sevilla. Mención especial a nuestros queridos Morgaz, que ha competido de manera brillante acercándose a las 2h30' (2h36'42''), y a Moises y Jairo por completar su primera maratón",
         "imagen": "img/maraton1.png"
-        },
+        }
     
-]
+];
