b9801bc5cbf3bbce41b92b1e1023f8d9c67d1fe4
ccrdferroviario <aruizrubio@gmail.com>
2026-07-05 12:05:19 +0200

    Update noticias.js

--- noticias.js
+++ noticias.js
@@ -12,12 +12,12 @@ const misNoticias = [
         "titulo": "¡Alta participación en el Maratón de Sevilla!",
         "fecha": "15 de Febrero, 2026",
         "resumen": "Hasta 11 corredores se han dado cita en la gran distancia del atletismo.", 
-        "contenido": "Hasta 11 corredores se han dado cita en la gran distancia del atletismo. Morgaz, Dorado (3h16'29''), Cecilio (3h24'11''), Adorna (3h31'50''), Castilla (3h36'49''), Iván (3h40'58''), Ignacio (3h42'56''), Antonio Dominguez (3h46'15''), Jairo (4h05'07''), Antonio Rafael (4h24'27'') y Moises (5h12'39'') han completado los 42,195km en un magnífico recorrido por las calles de Sevilla. Mención especial a nuestros queridos Morgaz, que ha competido de manera brillante acercándose a las 2h30' (2h36'42''), y a Moises y Jairo por completar su primera maratón",
+        "contenido": "Hasta 11 corredores se han dado cita en la gran distancia del atletismo. Morgaz, Dorado (3h16m29s), Cecilio (3h24m11s), Adorna (3h31m50s), Castilla (3h36m49s), Iván (3h40m58s), Ignacio (3h42m56s), Antonio Dominguez (3h46m15s), Jairo (4h05m07s), Antonio Rafael (4h24m27s) y Moises (5h12m39s) han completado los 42,195km en un magnífico recorrido por las calles de Sevilla. Mención especial a nuestros queridos Morgaz, que ha competido de manera brillante acercándose a las 2h30 (2h36m42s), y a Moises y Jairo por completar su primera maratón",
         "imagen": "img/maraton1.png"
-        },
+    },
     {
         "seccion": "ajedrez",
-        "titulo": "¡Vuelve el ajedrez al CCRD Ferroviario!",
+        "titulo": "¡Vuelve el Ajedrez al CCRD Ferroviario!",
         "fecha": "1 de Junio, 2026",
         "resumen": "Inauguramos la nueva sección de ajedrez con muchas novedades.",
         "contenido": "Inauguramos una nueva sección de ajedrez con muchas novedades y proyectos. ¡Anímate a entrenar tu mente y a disfrutar de la estrategia en el mejor ambiente!",
